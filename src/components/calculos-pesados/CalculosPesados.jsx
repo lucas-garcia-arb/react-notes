ac589bf82930bb97a6640160b7964002f52e5bc1
lucas-garcia-arb <lucas.garcia@arbusta.net>
2023-11-20 18:12:29 -0300

    Fix useMemo

--- src/components/calculos-pesados/CalculosPesados.jsx
+++ src/components/calculos-pesados/CalculosPesados.jsx
@@ -2,27 +2,34 @@ import { useMemo, useState } from "react";
 import { Button } from "react-bootstrap";
 
 export const CalculosPesados = () => {
-  const [listaNumeros, setListaNumeros] = useState([2, 3, 4, 5, 6, 7, 8, 9]);
+  const [listaNumeros, setListaNumeros] = useState([2]);
+
   const [show, setShow] = useState(true);
 
-  const getCalculo = useMemo(()=>{
-    return listaNumeros.reduce((a, b) => a * b)
-  },[listaNumeros]);
+  const getCalculo = useMemo(() => {
+    console.log("Realizar cálculo");
+    return listaNumeros.reduce((a, b) => a * b);
+  }, [listaNumeros]);
+
   const agregarNumero = () => {
     setListaNumeros([
       ...listaNumeros,
       listaNumeros[listaNumeros.length - 1] + 1,
     ]);
   };
+
   return (
     <>
       <h2>Cálculos Pesados</h2>
-      <p>Calculo:{getCalculo}</p>
+      {show ? <p>Calculo:{getCalculo}</p> : <p>Hidden</p>}
       <Button variant="primary" onClick={() => setShow(!show)}>
-        {show ? "Show" : "Hide"}
+        {!show ? "Show" : "Hide"}
       </Button>
       <Button variant="primary" onClick={() => agregarNumero()}>
         Agregar Número
+      </Button>
+      <Button variant="primary" onClick={() => setListaNumeros([2])}>
+        Reset
       </Button>
     </>
   );
